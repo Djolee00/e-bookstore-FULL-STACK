892d7a01b8a3fc8e5e5e7755f7d62716b1b958c4
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-02-11 11:58:42 +0100

    All routes specified

--- e-bookstore-laravel-backend/routes/api.php
+++ e-bookstore-laravel-backend/routes/api.php
@@ -17,3 +17,43 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('users', UserController::class);
+
+Route::resource('data', UserDataController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+
+Route::resource('orders', OrderController::class);
+Route::resource('items', OrderItemController::class);
+Route::resource('orders.items', ItemsByOrderController::class)->only('index');
+
+Route::resource('authors', AuthorController::class);
+Route::resource('categories', CategoryController::class);
+
+Route::resource('books', BookController::class)->only(['index', 'show']);;
+Route::resource('authors.books', AuthorBookController::class)->only('index');
+Route::resource('categories.books', CategoryBookController::class)->only('index');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::group(['middleware' => ['admin']], function () {
+    });
+
+    Route::resource('books', BookController::class)->only(['update', 'store', 'destroy'])->middleware('admin');
+
+    Route::resource('authors', AuthorController::class)->only(['update', 'store', 'destroy'])->middleware('admin');
+
+    Route::resource('favbooks', FavBookController::class)->only(['index', 'show', 'store', 'destroy']);
+
+    Route::resource('orders', OrderController::class)->only('index')->middleware('admin');;
+
+    Route::put('/update-user', [AuthController::class, 'update']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
